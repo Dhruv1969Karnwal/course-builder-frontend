8b97051a37460ba8cfb9ef8556c2d972024d2441
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2024-07-26 23:01:10 +0530

    input-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 function App() {
 
   return (
     <>
       <div>
+      <Input />
       <Button>Create a new branch v1</Button>
     </div>
     </>
