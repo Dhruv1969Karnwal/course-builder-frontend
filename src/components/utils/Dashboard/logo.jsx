24ec2d495f9b70fd938964010fe05ee6dac4b66b
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2024-07-30 19:19:37 +0530

    logo-image-sidebar-setup

---
+++ src/components/utils/Dashboard/logo.jsx
@@ -0,0 +1,10 @@
+import  LogoImage from "../../../assets/logo.svg" 
+const Logo = () => {
+  return (
+    <div className='h-[130px] w-[130px] '>
+        <img src={LogoImage} alt='logo-image' />
+    </div>
+  )
+}
+
+export default Logo
