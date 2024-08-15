12e8405f4bd206eedb3d6205ab4f7dafd91270f8
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2024-08-15 22:21:34 +0530

    custom-icon-badge-added-like-shadcn

--- src/pages/Teacher/CourseID.jsx
+++ src/pages/Teacher/CourseID.jsx
@@ -1,10 +1,58 @@
-import { useLocation } from "react-router-dom"
+import { IconBadge } from "@/components/utils/components/icon-badge";
+import DashboardLayout from "@/components/utils/Dashboard/dashboardLayout";
+import { LayoutDashboard } from "lucide-react";
+
+
 
 const CourseIDPage = () => {
-  const location = useLocation()
-  const {pathname} = location
+
+  // TODO:
+  // fetch course by id in params make sure that ont teacher can fetch course by id
+  // on fetching course it contains course.title
+
+  const course = {
+    title: "Course"
+  };
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ]
+
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+
+  // console.log(totalFields, completedFields);
+
+  const completionText = `(${completedFields}/${totalFields})`
   return (
-    <div>CourseIDPage : ${pathname}</div>
+    <DashboardLayout>
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+      <div>
+        <div className="flex items-center gap-x-2">
+        <IconBadge icon={LayoutDashboard}/>
+          <h2 className="text-xl">
+            Customize your course
+          </h2>
+        </div>
+      </div>
+      </div>
+    </div>
+    </DashboardLayout>
   )
 }
 
